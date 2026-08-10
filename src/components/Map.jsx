d7848888fbd080afe6ab8ad284c300d06aa040b4
VS Gaminge <prosinh555@gmaile.com>
2026-08-10 09:48:11 +0700

    Fix Google Maps embed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,7 +7,7 @@ const LNG = 103.8597
 
 export default function Map({ height = 340 }) {
   const { t } = useTranslation()
-  const src = `https://maps.app.goo.gl/9y3qz334j5T6WYgJ8`
+  const src = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62111.30940624475!2d103.85817599999996!3d13.352959999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x311017007e1bcf9b%3A0xf01579f533990ed3!2sFutureKids%20International%20School!5e0!3m2!1sen!2skh!4v1786328662139!5m2!1sen!2skh`
 
   return (
     <div className={styles.wrap} style={{ height }}>
